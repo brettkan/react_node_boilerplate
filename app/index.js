333b87fe82237a8889a059dd115854926b983ee0
brettkan <bkfighton@gmail.com>
2018-09-10 14:57:08 -0700

    Begin adding React

---
+++ app/index.js
@@ -0,0 +1,14 @@
+const React = require('react')
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello, world!</h1>
+        )
+    }
+}
+
+ReactDOM.render(
+    App,
+    document.getElementById('react-render')
+)
