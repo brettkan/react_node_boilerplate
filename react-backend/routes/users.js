ac0d00455860b27b749346502546178a79cbc5ea
brettkan <bkfighton@gmail.com>
2018-09-10 15:34:00 -0700

    Remove semicolons, switch to 4 space indent

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json([{
-    id: 1,
-    username: "brettkan"
-  }, {
-    id: 2,
-    username: "spiderman"
-  }]);
-});
+    res.json([{
+        id: 1,
+        username: "brettk"
+    }, {
+        id: 2,
+        username: "spiderman"
+    }])
+})
 
-module.exports = router;
+module.exports = router
